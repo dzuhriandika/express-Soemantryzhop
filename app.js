b29bf62a4d898e5aeecabfe26e1a7be0a18ec750
dzuhriandika <dzuhriandika@gmail.com>
2019-10-08 10:23:39 +0700

    add feature CRUD

--- app.js
+++ app.js
@@ -1,30 +1,69 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var flash = require('express-flash');
+var session = require('express-session');
+var index = require('./routes/index');
+var users = require('./routes/users');
+var customers = require('./routes/customers');
+var expressValidator = require('express-validator');
+var methodOverride = require('method-override');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var connection  = require('express-myconnection'); 
+var mysql = require('mysql');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({secret:"secretpass123456"}));
+app.use(flash());
+app.use(expressValidator());
+app.use(methodOverride(function(req, res){
+ if (req.body && typeof req.body == 'object' && '_method' in req.body) 
+  { 
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
+    } 
+  }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/*------------------------------------------
+    connection peer, register as middleware
+    type koneksi : single,pool and request 
+-------------------------------------------*/
+app.use(
+    connection(mysql,{
+        host: 'localhost',
+        user: 'root', // your mysql user
+        password : 'mataharii', // your mysql password
+        port : 3306, //port mysql
+        database:'nodejs' // your database name
+    },'pool') //or single
+
+);
+
+app.use('/', index);
+app.use('/customers', customers);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/blankpage', function(req, res, next) {
+  res.render('blankpage', { title: 'Express' });
+});
+  
 module.exports = router;
